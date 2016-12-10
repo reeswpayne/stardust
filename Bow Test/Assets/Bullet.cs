13210a05223b1858c3cc3776deee111e936ee0fe
Rees Payne <rplegos@gmail.com>
2016-12-09 22:37:37 -0500

    Add files via upload

---
+++ Bow Test/Assets/Bullet.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        Destroy(gameObject, 2.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.right * Time.deltaTime * 10);
+        
+    }
+
+}
---
+++ Bow Test/Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+    // Variables
+    public GameObject bullet;
+    float timer = 120;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timer += 1;
+        if (timer >= 120)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+
+                Instantiate(bullet, transform.position, transform.rotation);
+            }
+        }
+    }
+}
